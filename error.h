b69247fa5abf11a4b1af3397fc212316ced4c90d
Oleg 'cNoNim' Ageev <my@cnonim.name>
2015-04-04 22:35:17 +0300

    Fix MemoryCheck

--- error.h
+++ error.h
@@ -5,9 +5,9 @@
 
 #define ErrorMsg(filename, lineno, format, ...) lprintf(LERROR, "%s:%d:"#format, filename, lineno, ## __VA_ARGS__)
 
-#define MemoryCheck(X)                            \
-  if ((X) == 0) {                                 \
-    lprintf(LERROR, "Out of memory.  Aborting..."); \
+#define MemoryCheck(X)                              \
+  if (!(X)) {                                         \
+    lprintf(LFATAL, "Out of memory.  Aborting..."); \
   }
 
 #endif //_LEMON_ERROR_H_
